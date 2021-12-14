5aa83f1be25a304d6216cd6b72107349c9486def
moruneku <1216archer@gmail.com>
2021-12-14 22:31:14 +0900

    適切なコミットコメント

---
+++ blog/app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+　　public function index(Post $post)
+{
+    return $post->get();
+}
+}
+
